bbde4170a246d0cc1f68a4b737af6e93200d5344
jeffryporter <jporter7@gmail.com>
2016-06-07 10:27:51 -0400

    Add session

--- src/com/theironyard/javawithclojure/porter/Main.java
+++ src/com/theironyard/javawithclojure/porter/Main.java
@@ -1,9 +1,11 @@
 package com.theironyard.javawithclojure.porter;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -16,24 +18,31 @@ import java.util.HashMap;
     // GET: routes return html
     // POST: routes modify the server
 
-public class Main
-{
-    static User user;
+public class Main{
+
+    static HashMap<String, User> users = new HashMap<>();
+    static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args)
     {
+
+
         Spark.init();
         Spark.get(
                 "/",
-                (request, response) -> {
+                (request, response) ->
+                {
+                    Session session = request.session();
+                    String username = session.attribute("username");
                     HashMap m = new HashMap();
-                    if (user == null)
+                    if (username == null)
                     {
                         return new ModelAndView(m, "login.html");
                     }
                     else
                     {
-                        m.put("name", user.name);
+                        m.put("name", username);
+                        m.put("users", userList);
                         return new ModelAndView(m, "home.html");
                     }
                 },
@@ -43,9 +52,27 @@ public class Main
                 "/login",
                 (request,response) -> {
                     String username = request.queryParams("username");
-                    user = new User(username);
+                    User user = users.get(username);
+                    if (user == null)
+                    {
+                        user = new User(username);
+                        users.put(username, user);
+                        userList.add(user);
+                    }
+                    Session session = request.session();
+                    session.attribute("username", username);
+
                     response.redirect("/");
                     return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return"";
                 }
         );
     }
